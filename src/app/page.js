acc2a269bca8c1061e6d22a25b70af0dbe6a749a
NIKHIL MURALI <Nikhil Murali>
2023-09-23 18:47:49 +0530

    frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import cover from 'public/cover.avif'
 
 export default function Home() {
   return (
     <>
-    <div className={styles.main}> home</div>
+    <div className={styles.main}> 
+    <div className={styles.textcontainer}>
+      Fin4U
+      <div className={styles.subtextcontainer}>
+      The Financial support for you
+      </div>
+    </div>
+    <div className={styles.imagecontainer}>
+    <Image src={cover} className={styles.img} alt="college"></Image>
+    </div>
+    </div>
     </>
   )
 }
